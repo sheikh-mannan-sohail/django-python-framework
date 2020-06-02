372fab23cdd11fe571225184ade2308426de12ed
sheikh-mannan-sohail <sheikhmannansohail@gmail.com>
2020-06-02 18:24:43 +0530

    Added new endpoints to ecommerce site

--- Django/awesome_cart/shop/urls.py
+++ Django/awesome_cart/shop/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 
-from .views import index
+from . import views
 
 urlpatterns = [
-    path('', index),
+    path('', views.index, name="ShopIndex"),
+    path('about/', views.about, name="AboutUs"),
+    path('contact/', views.contact, name="ContactUs"),
+    path('tracker/', views.tracker, name="TrackStatus"),
+    path('search/', views.search, name="Search"),
+    path('product_view/', views.prod_view, name="ProductView"),
+    path('checkout/', views.checkout, name="Checkout"),
 ]
--- Django/awesome_cart/shop/views.py
+++ Django/awesome_cart/shop/views.py
@@ -1,6 +1,32 @@
+from django.http import HttpResponse
+
 from django.shortcuts import render
 
 
 # Create your views here.
 def index(request):
     return render(request, "shop/index.html")
+
+
+def about(request):
+    return HttpResponse("About our e-cart service")
+
+
+def contact(request):
+    return HttpResponse("Contact our e-cart service")
+
+
+def tracker(request):
+    return HttpResponse("Track your order status ")
+
+
+def search(request):
+    return HttpResponse("Search what you need")
+
+
+def prod_view(request):
+    return HttpResponse("Product view ")
+
+
+def checkout(request):
+    return HttpResponse(" Checkout status")
